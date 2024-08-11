5ba99d0a88791b9075df4d8ad738d1e585373893
kalicyh <34980061+kaliCYH@users.noreply.github.com>
2024-08-11 14:39:17 +0800

    🧪 test: 上传组件

--- src/views/database/UpLoad.vue
+++ src/views/database/UpLoad.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import UiParentCard from '@/components/shared/UiParentCard.vue';
+import UpLoad from '@/components/shared/WidgetUpLoad.vue';
 </script>
 <template>
     <v-row>
         <v-col cols="12" md="12">
             <UiParentCard title="数据上传"> 
-                <div class="pa-7 pt-1"><p class="text-body-1">test</p></div>
+                <UpLoad/>
             </UiParentCard>
         </v-col>
     </v-row>
