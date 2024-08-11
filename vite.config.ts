83c0d281ad973734632275ba1ef9f5829b0d0ad6
kalicyh <34980061+kaliCYH@users.noreply.github.com>
2024-08-11 19:06:53 +0800

    🎈 perf: baseurl

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vuetify from "vite-plugin-vuetify";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/dist',
+  base: '/dist/',
   plugins: [
     vue(),
     vuetify({
